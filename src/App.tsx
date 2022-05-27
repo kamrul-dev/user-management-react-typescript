491886c55dd9b28b299d85a0b67163c2c6a1b232
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-28 02:13:47 +0600

    Basic types number string boolean interface~

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let student: string = 'Kamrul';
+let age: number = 69;
+let isSmart: boolean = true;
+
+let students: string[] = ['kamrul', 'hasan', 'babu'];
+
+let fees: number[] = [12, 45, 78, 12, 45];
+
+interface Person{
+  name: string,
+  job?: string,
+  age: number
+}
+
+const person: Person = {
+  name: 'Bill',
+
+  age: 85
+
+}
+/* const person: {name: string, job: string, age: number} = {
+  name: 'Bill',
+  job: 'jobless',
+  age: 85
+} */
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/vanila/vanila.js
@@ -0,0 +1,8 @@
+let user = 'Kamrul';
+user = 55;
+user = false;
+user = { id: 1, age: 25, address: 'K' }
+user = [1, 2, 3, 4, 5, 6];
+user = ['kamrul', 'hasan', 'babu'];
+user = () => 55;
+console.log(user);
